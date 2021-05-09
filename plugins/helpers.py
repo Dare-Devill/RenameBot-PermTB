4dd615a6c9c1c10ddf9630a522c3de8072d7596a
Dare-Devill <70199827+Dare-Devill@users.noreply.github.com>
2021-05-09 22:08:48 +0530

    Update helpers.py

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -25,7 +25,7 @@ async def progress_for_pyrogram(
 
         progress = "[{0}{1}] \n\n☢️Progress: {2}%\n".format(
             ''.join(["🟢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⚫" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["⬜" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\n\n️🚄Speed: {2}/s\n\n⏳ETA: {3}\n".format(
