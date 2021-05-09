3a3139477f71a7595143d331b3a706563bc566ae
Dare-Devill <70199827+Dare-Devill@users.noreply.github.com>
2021-05-09 21:57:49 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,7 +30,7 @@ def help_user(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.HELP_USER,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact DEV ⭕️", url="https://t.me/its_prathxm")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔥 Support Group 🔥", url="https://t.me/Pbotsupport")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
